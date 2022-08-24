6c334a5bb7e0b7bdefc841cedc9d58ab4ae610fe
Lawlesh2022 <106768533+Lawlesh2022@users.noreply.github.com>
2022-08-24 21:56:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
+const addText=()=>{
+  const input = [...text];
+  setText(input)
 
 function App() {
+  const[text, setText] = useState("")
   return (
     <div id="main">
-      // Do not alter the main div
+    <input type = "text" onChange={(e)=>setText(e.target.value)}/>
+    <button id="click" onClick ={addText}></button> 
     </div>
   );
 }
