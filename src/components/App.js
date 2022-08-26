18ea97ba28a5e6e76b0bd73d63f9ce0fcb4ac40f
Lawlesh2022 <106768533+Lawlesh2022@users.noreply.github.com>
2022-08-26 19:13:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,16 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 function App() {
-  const[text, setText] = useState("")
-  const addText =()=>{
-    setText("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
-  }
-  return (
-    <div id="main">
-    <p id = "para">{text}</p>
-    <button id="click" onClick ={addText}>addText</button> 
-    </div>
-  );
+    const[text, setText] = useState("")
+    const addText =()=>{
+        setText("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
+    }
+    return (
+        <div id="main">
+            <p id = "para">{text}</p>
+            <button id="click" onClick ={addText}>addText</button> 
+        </div>
+   )
 }
-
 
 export default App;
