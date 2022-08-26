43b55ee52eecf0cb4ef36e8ebe106f5199ee099e
Lawlesh2022 <106768533+Lawlesh2022@users.noreply.github.com>
2022-08-26 19:01:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,11 @@ function App() {
   const[text, setText] = useState("")
   const addText =()=>{
     setText("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
+  }
   return (
     <div id="main">
     <p id = "para">{text}</p>
-    <button id="click" onClick ={addText}></button> 
+    <button id="click" onClick ={addText}>addText</button> 
     </div>
   );
 }
