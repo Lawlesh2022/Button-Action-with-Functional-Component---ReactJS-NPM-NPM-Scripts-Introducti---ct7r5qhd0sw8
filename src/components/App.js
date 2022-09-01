4cde10118bc3de96e29e85a7c84d9b2f85d82f3f
Lawlesh2022 <106768533+Lawlesh2022@users.noreply.github.com>
2022-09-02 00:28:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,18 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 function App() {
     const [text, setText] = useState("")
-//     const [showpara, setShowpara] = useState(false)
+     const [showpara, setShowpara] = useState(false)
    
     const enablePara = ()=>{
-//         setShowpara(true)   
+        setShowpara(true)   
         setText("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")           
     }
     return (
         <div id="main">
-        
-//             showpara ?
-            <p id="para">{text}</p>
-            
+        {
+           showpara ?
+           <p id="para">{text}</p>:null
+        }   
             <button id="click" onClick ={enablePara}>addText</button> 
         </div>
    )
